20b6e0f19c92ec47d27df8dd35b26841e8027743
Juan André <juuanandre722@gmail.com>
2025-07-02 17:12:27 -0300

    teste(uconnect): rede educ_funcional

--- backend/database/db.js
+++ backend/database/db.js
@@ -22,6 +22,15 @@ const initDB = async () => {
       senha TEXT NOT NULL
     )`);
 
+    db.run(`CREATE TABLE IF NOT EXISTS estagiarios (
+
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      avatar TEXT,
+      nome TEXT NOT NULL,
+      email TEXT UNIQUE NOT NULL,
+      senha TEXT NOT NULL,
+      )`)
+
     db.run(`CREATE TABLE IF NOT EXISTS posts (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       avatar TEXT,
@@ -104,6 +113,17 @@ const initDB = async () => {
       avatar: "paulo_avatar.jpg",
       turmas: [5],
     },
+  ];
+
+  
+  const estagiarios = [
+    {
+      nome: "Juan André",
+      email: "juan@utec.com",
+      senha: "123456",
+      avatar: "",
+      turmas: [1, 3],
+    }
   ];
 
   for (const prof of professores) {
